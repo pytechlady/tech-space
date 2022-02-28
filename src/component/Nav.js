e4bd924f1aa6daf196e501619795d882c1aff875
newbietechlady <fumex9910@gmail.com>
2022-02-28 03:34:23 +0100

    fixed navbar active link

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Logo from './Logo';
 import '../App.css';
 
 const Nav = () => {
+  const location = useLocation();
+
+    const { pathname } = location;
+
+    const splitLocation = pathname.split("/");
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light mb-5 p-4 fixed-top" style={{backgroundColor: "blue"}}>
   <div className="container">
@@ -15,21 +21,25 @@ const Nav = () => {
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item ms-5">
-        <Link className="nav-link text-light nav-el" to="/">
-            Home
+
+      <li className="ms-5">
+        <Link exact className={splitLocation[1] === "" ? "active" : "links"} to="/">
+        Home
           </Link>
-        </li>
-        <li className="nav-item ms-5">
-          <Link className="nav-link text-light nav-el" to="/services">
-            Services
+          </li>
+        
+          <li className="ms-5">
+          <Link className={splitLocation[1] === "services" ? "active" : "links"} to="/services">
+          Services
           </Link>
-        </li>
-        <li className="nav-item ms-5">
-          <Link className="nav-link text-light nav-el" to='/contact'>
-            Contact
+          </li>
+        
+          <li className="ms-5">
+          <Link className={splitLocation[1] === "contact" ? "active" : "links"} to='/contact'>
+          Contact
           </Link>
-        </li>
+          </li>
+        
       </ul>
     </div>
   </div>
